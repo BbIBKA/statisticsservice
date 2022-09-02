a79d1690d54f4d8a234002a617d5f193f8bb518e
Денис Шестернин <dshesternin@gmail.com>
2022-09-02 14:40:16 +0300

    jacoco test commit

---
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -0,0 +1,29 @@
+package ru.netology.statistic;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StatisticsServiceTest {
+    @Test
+    void findMax() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxNewData() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {5, 7, 10, 14, 15, 13, 18, 16, 11, 11, 12};
+        long expected = 18;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+}
